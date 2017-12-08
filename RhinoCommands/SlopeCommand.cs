bb425b2608e44f0bbaa06cbcd72709aaa4dbf5c7
jmorrison150 <jomo150@gmail.com>
2017-12-08 06:38:38 -0600

    Slope

--- RhinoCommands/SlopeCommand.cs
+++ RhinoCommands/SlopeCommand.cs
@@ -21,40 +21,44 @@ namespace RhinoCommands {
 
         ///<returns>The command name as it appears on the Rhino command line.</returns>
         public override string EnglishName {
-            get { return "SlopeCommand"; }
+            get { return "Slope"; }
         }
 
         protected override Result RunCommand(RhinoDoc doc, RunMode mode) {
             // TODO: start here modifying the behaviour of your command.
             // ---
-            RhinoApp.WriteLine("The {0} command will add a line right now.", EnglishName);
+            RhinoApp.WriteLine("The {0} command will calculate very basic overall slope of a curve.", EnglishName);
 
-            Point3d pt0;
-            using (GetPoint getPointAction = new GetPoint()) {
-                getPointAction.SetCommandPrompt("Please select the start point");
-                if (getPointAction.Get() != GetResult.Point) {
-                    RhinoApp.WriteLine("No start point was selected.");
-                    return getPointAction.CommandResult();
-                }
-                pt0 = getPointAction.Point();
-            }
+            //GetInteger
+            //GetLine
+            //GetNumber
+            //GetObject
+            //GetPoint
+            //GetString
+            //GetTransform
 
-            Point3d pt1;
-            using (GetPoint getPointAction = new GetPoint()) {
-                getPointAction.SetCommandPrompt("Please select the end point");
-                getPointAction.SetBasePoint(pt0, true);
-                getPointAction.DynamicDraw +=
-                  (sender, e) => e.Display.DrawLine(pt0, e.CurrentPoint, System.Drawing.Color.DarkRed);
-                if (getPointAction.Get() != GetResult.Point) {
-                    RhinoApp.WriteLine("No end point was selected.");
-                    return getPointAction.CommandResult();
-                }
-                pt1 = getPointAction.Point();
-            }
 
-            doc.Objects.AddLine(pt0, pt1);
-            doc.Views.Redraw();
-            RhinoApp.WriteLine("The {0} command added one line to the document.", EnglishName);
+           
+
+            const Rhino.DocObjects.ObjectType geometryFilter = Rhino.DocObjects.ObjectType.Curve;
+            GetObject getCurve = new GetObject();
+            getCurve.SetCommandPrompt("selece a curve");
+            getCurve.GeometryFilter = geometryFilter;
+            getCurve.GroupSelect = false;
+            getCurve.SubObjectSelect = false;
+            getCurve.EnableClearObjectsOnEntry(false);
+            getCurve.DeselectAllBeforePostSelect = false;
+
+            GetResult res = getCurve.Get();
+            Rhino.DocObjects.RhinoObject rhinoObject = getCurve.Object(0).Object();
+            Curve c = (Curve) rhinoObject.Geometry;
+
+            double rise = c.PointAtEnd.Z - c.PointAtStart.Z;
+            double run = c.GetLength();
+            if (run==0) { return Result.Nothing; }
+            double slope = rise / run;
+
+            RhinoApp.WriteLine("Slope = " + slope.ToString());
 
             // ---
 
---
+++ RhinoCommands/SlopeMax.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Rhino;
+using Rhino.Commands;
+
+namespace RhinoCommands {
+    public class SlopeMax : Command {
+        static SlopeMax _instance;
+        public SlopeMax() {
+            _instance = this;
+        }
+
+        ///<summary>The only instance of the SlopeMax command.</summary>
+        public static SlopeMax Instance {
+            get; private set;
+        }
+
+        public override string EnglishName {
+            get { return "SlopeMax"; }
+        }
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode) {
+            // TODO: complete command.
+            RhinoApp.WriteLine("Slope = Max");
+
+            return Result.Success;
+        }
+    }
+}
